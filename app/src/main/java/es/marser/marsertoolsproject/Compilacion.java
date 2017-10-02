e2b0571be79ee4749a2d7e831a5ba9e9a04b6b04
sergio <spseligrat@gmail.com>
2017-10-02 19:00:17 +0200

    Compilación de jbasictools

---
+++ app/src/main/java/es/marser/marsertoolsproject/Compilacion.java
@@ -0,0 +1,16 @@
+package es.marser.marsertoolsproject;
+
+import es.marser.marsertoolslib.jbasicutils.TextTools;
+
+/**
+ * @author sergio
+ *         Created by sergio on 2/10/17.
+ *         Pruebas de compilación de librerías
+ */
+
+public class Compilacion {
+
+    public void init() {
+        TextTools.nc(null);
+    }
+}
